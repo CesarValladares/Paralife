c898909466d9e8b8dd15c19dc3fe47cc868b424f
maxsegovia <maxsegoviamtz@gmail.com>
2018-08-20 10:05:43 -0500

    Cambios

--- script.js
+++ script.js
@@ -8,13 +8,6 @@ $(document).ready(function() {
     $('.closeModal').on("click", function() {
         $('.modal').css('display', 'none');
     });
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == $('#myModal')) {
-            $('#myModal').css('display', 'none');
-        }
-    }
 });
 
 $(function() {
